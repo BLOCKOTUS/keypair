97252f75f01f62699ca921abbe10f46766e98fb4
Daniel Febrero <febrero@me.com>
2021-01-27 18:32:38 +0100

    upgrade packages and linted

--- api/lib/index.ts
+++ api/lib/index.ts
@@ -9,73 +9,73 @@ const WALLET_PATH = path.join(__dirname, '..', '..', '..', '..', 'wallet');
  * Store a `Keypair` and eventually make it readable by other users.
  */
 export const share = async ({
-	sharedWith,
-	groupId,
-	myEncryptedKeyPair,
-	type,
-	user,
+  sharedWith,
+  groupId,
+  myEncryptedKeyPair,
+  type,
+  user,
 }) => {
-	return new Promise(async (resolve, reject) => {
-		// create wallet
-		const walletPath = path.join(WALLET_PATH, `${user.username}.id`);
-		fs.writeFileSync(walletPath, JSON.stringify(user.wallet));
+  return new Promise(async (resolve, reject) => {
+    // create wallet
+    const walletPath = path.join(WALLET_PATH, `${user.username}.id`);
+    fs.writeFileSync(walletPath, JSON.stringify(user.wallet));
 
-		// get contract, submit transaction and disconnect
-		var {contract, gateway} = await 
-			getContractAndGateway({username: user.username, chaincode: 'keypair', contract: 'Keypair'})
+    // get contract, submit transaction and disconnect
+    const {contract, gateway} = await
+      getContractAndGateway({username: user.username, chaincode: 'keypair', contract: 'Keypair'})
         .catch(reject);
-        
-    if (!contract || !gateway) return;
 
-		var response = await 
-			contract
-				.submitTransaction('createSharedKeypair', JSON.stringify(sharedWith), groupId, myEncryptedKeyPair, type)
-        .catch(reject);    
+    if (!contract || !gateway) { return; }
+
+    const response = await
+      contract
+        .submitTransaction('createSharedKeypair', JSON.stringify(sharedWith), groupId, myEncryptedKeyPair, type)
+        .catch(reject);
 
     await gateway.disconnect();
-    
-    if (!response) return;
+
+    if (!response) { return; }
 
     console.log('Transaction has been submitted', response);
-		resolve();
-		return;
-	});
+    resolve();
+    return;
+  });
 };
 
 /**
  * Retrieve a `Keypair` from the network.
  */
 export const get = async ({
-	keypairId,
-	user,
+  keypairId,
+  user,
 }) => {
-	return new Promise(async (resolve, reject) => {
-		// create wallet
-		const walletPath = path.join(WALLET_PATH, `${user.username}.id`);
-		fs.writeFileSync(walletPath, JSON.stringify(user.wallet));
+  return new Promise(async (resolve, reject) => {
+    // create wallet
+    const walletPath = path.join(WALLET_PATH, `${user.username}.id`);
+    fs.writeFileSync(walletPath, JSON.stringify(user.wallet));
 
-		// get contract, submit transaction and disconnect
-		var {contract, gateway} = await 
-			getContractAndGateway({username: user.username, chaincode: 'keypair', contract: 'Keypair'})
+    // get contract, submit transaction and disconnect
+    const {contract, gateway} = await
+      getContractAndGateway({username: user.username, chaincode: 'keypair', contract: 'Keypair'})
         .catch(reject);
-        
-    if (!contract || !gateway) return;
 
-		// submit transaction
-		const rawKeypair = await 
-			contract
-				.submitTransaction('getKeypair', keypairId)
+    if (!contract || !gateway) { return; }
+
+    // submit transaction
+    const rawKeypair = await
+      contract
+        .submitTransaction('getKeypair', keypairId)
         .catch(reject);
-        
-    //disconnect
+
+    // disconnect
     await gateway.disconnect();
 
-    if (!rawKeypair) return;
+    if (!rawKeypair) { return; }
 
-		const keypair = JSON.parse(rawKeypair.toString('utf8'));
+    const keypair = JSON.parse(rawKeypair.toString('utf8'));
 
-		console.log('Transaction has been submitted');
-		resolve(keypair);
+    console.log('Transaction has been submitted');
+    resolve(keypair);
     return;
-	});
+  });
 };
